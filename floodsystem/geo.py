666099c1e7857729aaa59de746c58b1a1046ecd6
JoeBarney1 <98259645+JoeBarney1@users.noreply.github.com>
2022-01-30 16:09:13 +0000

    commit

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -6,4 +6,13 @@ geographical data.
 
 """
 
+from importlib_metadata import import_module
 from .utils import sorted_by_key  # noqa
+from floodsystem.stationdata import build_station_list
+import haversine
+def stations_by_distance(stations, p):
+    stations= build_station_list()
+    station_distances=[]
+    for station in stations:
+        distance=float(haversine(p,))
+        station_distances += (station, distance)
