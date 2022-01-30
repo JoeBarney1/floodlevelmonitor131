4a212821e1e242c1bc5b80f7722ec946e439e750
JoeBarney1 <98259645+JoeBarney1@users.noreply.github.com>
2022-01-30 18:07:54 +0000

    push

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -13,7 +13,7 @@ def stations_by_distance(stations, p):
     station_distances=[] #empty list for station name and dist from p
     for station in stations:
         distance=float(haversine(p,station.coord)) #set distance for each loop as dist from p
-        station_distances += (station.name, distance) #adds a tuple of a station name and it's distance from p to empty list
+        station_distances.append((station.name, distance)) #adds a tuple of a station name and it's distance from p to empty list
     sorted_station_distances = sorted_by_key(station_distances, int(1)) #sorts list based on second item i.e distance not name
     return sorted_station_distances
     
