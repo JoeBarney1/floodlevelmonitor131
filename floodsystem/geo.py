f3f80be159741eac2724c554a626f844031b4da6
JoeBarney1 <98259645+JoeBarney1@users.noreply.github.com>
2022-01-30 17:59:01 +0000

    commit

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -5,14 +5,15 @@
 geographical data.
 
 """
-
 from importlib_metadata import import_module
-from .utils import sorted_by_key  # noqa
+from haversine import haversine, Unit 
+from floodsystem.utils import sorted_by_key# noqa
 from floodsystem.stationdata import build_station_list
-import haversine
-def stations_by_distance(stations, p):
-    stations= build_station_list()
-    station_distances=[]
+def stations_by_distance(stations, p): 
+    station_distances=[] #empty list for station name and dist from p
     for station in stations:
-        distance=float(haversine(p,))
-        station_distances += (station, distance)
+        distance=float(haversine(p,station.coord)) #set distance for each loop as dist from p
+        station_distances += (station.name, distance) #adds a tuple of a station name and it's distance from p to empty list
+    sorted_station_distances = sorted_by_key(station_distances, int(1)) #sorts list based on second item i.e distance not name
+    return sorted_station_distances
+    
