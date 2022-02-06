a0211f7d4832df2bb675137d8c432e1445cae73f
Louis Burrows <burrowsbram@live.com>
2022-02-06 18:37:43 +0000

    Updated tasks.

--- Task1C.py
+++ Task1C.py
@@ -3,6 +3,9 @@ from sympy import N
 from floodsystem.geo import stations_within_radius
 from floodsystem.stationdata import build_station_list
 
+def run():
+    cam = (52.2053, 0.1218)
+    print(stations_within_radius(build_station_list(),cam, 10))
 
-cam = (52.2053, 0.1218)
-print(stations_within_radius(build_station_list(),cam, 10))
+if __name__ == "__main__":
+    run()
--- Task1E.py
+++ Task1E.py
@@ -3,4 +3,8 @@ from sympy import N
 from floodsystem.geo import rivers_by_station_number
 from floodsystem.stationdata import build_station_list
 
-print(rivers_by_station_number(build_station_list(), 9))
+def run():
+    print(rivers_by_station_number(build_station_list(), 9))
+
+if __name__ == "__main__":
+    run()
