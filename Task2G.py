e3c96907011846e627ef5827bae30513d275c1b5
JoeBarney1 <98259645+JoeBarney1@users.noreply.github.com>
2022-03-02 08:03:05 +0000

    commit

--- Task2G.py
+++ Task2G.py
@@ -4,7 +4,7 @@ from floodsystem.stationdata import build_station_list
 def run():
     """Requirements for Task 2G"""
     stations= build_station_list()
-    print(highest_risk(stations[0:400],dt=3,N=10,y=3))
+    print(highest_risk(stations,dt=3,N=10,y=3))
     #works with whole list but takes v. long time
 
 
